214e6bb9c625fee5671632a3547b3c570010729e
Jawahir-j-x <156565027+Jawahir-j-x@users.noreply.github.com>
2024-01-15 11:13:06 +0000

    dataTypes

---
+++ .devcontainer/01.basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strick " //treat all Js code as newer version
+// alert( 3 + 3) we are usin nodejs, not browser
+
+console.log(3
+     + 
+     3) // code readability  should be high
+
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// Object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+
+
+
