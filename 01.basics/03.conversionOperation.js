2eb9cbfdd188957922906a0b19df0f63d240f5bc
Jawahir-j-x <156565027+Jawahir-j-x@users.noreply.github.com>
2025-01-15 05:01:25 +0000

    conversionOperation

--- 01.basics/03.conversionOperation.js
+++ 01.basics/03.conversionOperation.js
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "Hitesh"
 
-let booleanIsLoggedI = boolean(isLoggedIn)
+// let booleanIsLoggedI = boolean(isLoggedIn)
 // console.log(booleanIsLoggedI);
 
 // 1 => true; 0 => false
@@ -53,6 +53,14 @@ let str3 = str1 + str2
 
 // console.log( (3 + 4) * 5 % 3);
 
-console.log(true);
+// console.log(true);
+// console.log(+"");
 
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100 
+++gameCounter;
+console.log(gameCounter);
 
